9e12158c5947589e28af8c349433598d6e1c5eef
Raphael <raphael.ezeigwe@yahoo.com>
2024-10-16 22:41:09 +0100

    Add Viewsets

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from . import serializers as s
+from . import models as m
 
-# Create your views here.
+class ProductViewSet(ModelViewSet):
+    queryset = m.Product.objects.all()
+    serializer_class = s.ProductSerializer
+
+class CategoryViewSet(ModelViewSet):
+    queryset = m.Category.objects.all()
+    serializer_class = s.CategorySerializer
+
